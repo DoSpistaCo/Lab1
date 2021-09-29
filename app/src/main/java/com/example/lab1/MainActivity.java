e64ca9725e0a2ae93b886b6b9194b333e68caea9
DoSpistaCo <xbasil488@gmail.com>
2021-09-29 11:24:49 +0300

    Added button

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.lab1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.tvMain = findViewById(R.id.tvMain);
     }
+
+    public void onClickChangeText(View view) {
+        this.tvMain.setText("TextView text has changed!!!");
+    }
+
+    //some next commit
 }
